d4c9da3dc2544005f3dce6b0cc0da0574ece9a22
zhangkw <zhangkw@tongji.edu.cn>
2026-08-21 16:23:03 +0800

    Refactor async tasks in deadlock benchmark
    - Renamed the original async task to `task1` and created a new async task `task2` to demonstrate different locking patterns.
    - Updated the main function to spawn the new task names, maintaining the benchmark's focus on async deadlock scenarios.

--- bench/deadlock/async-deadlock/src/main.rs
+++ bench/deadlock/async-deadlock/src/main.rs
@@ -2,7 +2,13 @@ use std::sync::Arc;
 
 use parking_lot::Mutex;
 
-async fn task(a: Arc<Mutex<i32>>, b: Arc<Mutex<i32>>) {
+async fn task1(a: Arc<Mutex<i32>>, b: Arc<Mutex<i32>>) {
+    let _ga = a.lock();
+    tokio::time::sleep(std::time::Duration::from_millis(10)).await;
+    let _gb = b.lock();
+}
+
+async fn task2(a: Arc<Mutex<i32>>, b: Arc<Mutex<i32>>) {
     let _ga = a.lock();
     tokio::time::sleep(std::time::Duration::from_millis(10)).await;
     let _gb = b.lock();
@@ -12,8 +18,8 @@ async fn task(a: Arc<Mutex<i32>>, b: Arc<Mutex<i32>>) {
 async fn main() {
     let l1 = Arc::new(Mutex::new(0));
     let l2 = Arc::new(Mutex::new(0));
-    let h1 = tokio::spawn(task(l1.clone(), l2.clone()));
-    let h2 = tokio::spawn(task(l2.clone(), l1.clone()));
+    let h1 = tokio::spawn(task1(l1.clone(), l2.clone()));
+    let h2 = tokio::spawn(task2(l2.clone(), l1.clone()));
     let _ = h1.await;
     let _ = h2.await;
 }
